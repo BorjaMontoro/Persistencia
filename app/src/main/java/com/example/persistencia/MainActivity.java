702a7a5e851c707069b930786e38fd6f054bbb94
Borja22 <bmontoroplaza.cf@iesesteveterradas.cat>
2022-11-04 19:24:09 +0100

    Creacion archivo

--- app/src/main/java/com/example/persistencia/MainActivity.java
+++ app/src/main/java/com/example/persistencia/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.persistencia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b=findViewById(R.id.boton);
+
+        try{
+            File dir= getFilesDir();
+            Log.i("Info",dir.toString());
+            File archivo= new File(dir,"dades.txt");
+            if(!archivo.exists()) {
+                archivo.createNewFile();
+            }
+            FileWriter archivoescribir=new FileWriter(dir+"/dades.txt");
+            Log.i("Info",dir+"/dades.txt");
+            String mensaje="Cataluña es la mejor comunidad autonoma de España";
+            archivoescribir.write(mensaje);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
