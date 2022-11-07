6d78381c8089317c75ff41687fe53725661694ad
Borja22 <bmontoroplaza.cf@iesesteveterradas.cat>
2022-11-07 17:57:06 +0100

    Añadido las funciones de actualizar archivo y el edit text (aplicacion casi acabada)

--- app/src/main/java/com/example/persistencia/MainActivity.java
+++ app/src/main/java/com/example/persistencia/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.persistencia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.OutputStream;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,18 +25,50 @@ public class MainActivity extends AppCompatActivity {
 
         try{
             File dir= getFilesDir();
-            Log.i("Info",dir.toString());
-            File archivo= new File(dir,"dades.txt");
-            if(!archivo.exists()) {
-                archivo.createNewFile();
-            }
-            FileWriter archivoescribir=new FileWriter(dir+"/dades.txt");
-            Log.i("Info",dir+"/dades.txt");
-            String mensaje="Cataluña es la mejor comunidad autonoma de España";
-            archivoescribir.write(mensaje);
+            File archivo=new File(dir,"dades.txt");
+            FileWriter escribir=new FileWriter(archivo);
+            String mensaje="Lorem Ipsum is simply dummy text of the printing and typesetting industry.\n";
+            escribir.write(mensaje);
+            escribir.flush();
+            escribir.close();
+            actualizarEditText();
+            b.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    actualizaArchivo();
+                    actualizarEditText();
+
+                }
+            });
 
         }catch (Exception e){
             e.printStackTrace();
+        }
+    }
+    private void actualizarEditText(){
+        File dir = getFilesDir();
+        File archivo = new File(dir,"dades.txt");
+        EditText edit=findViewById(R.id.editar);
+        edit.setText("");
+        try {
+            Scanner obj = new Scanner(archivo);
+
+            while (obj.hasNextLine())
+                edit.append(obj.nextLine()+"\n");
+        }
+        catch (IOException e) {
+        }
+    }
+    private void actualizaArchivo(){
+        try {
+            EditText edit=findViewById(R.id.editar);
+            File dir= getFilesDir();
+            File archivo=new File(dir,"dades.txt");
+            FileWriter escribir=new FileWriter(archivo);
+            escribir.write(edit.getText().toString());
+            escribir.flush();
+            escribir.close();
+        }catch(Exception e){
+
         }
     }
 }
